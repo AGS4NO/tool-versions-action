039145645fe38568b95607f3c6ad5543bd66fce5
Stephen Nelson <47795400+AGS4NO@users.noreply.github.com>
2024-03-02 11:07:39 -0500

    Initial release (#3)
    Prepare for initial release v1

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -12,9 +12,6 @@ import * as main from '../src/main'
 // Mock the action's main function
 const runMock = jest.spyOn(main, 'run')
 
-// Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/
-
 // Mock the GitHub Actions core library
 let debugMock: jest.SpiedFunction<typeof core.debug>
 let errorMock: jest.SpiedFunction<typeof core.error>
@@ -33,12 +30,12 @@ describe('action', () => {
     setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
   })
 
-  it('sets the time output', async () => {
+  it('sets the tool versions output', async () => {
     // Set the action's inputs as return values from core.getInput()
     getInputMock.mockImplementation(name => {
       switch (name) {
-        case 'milliseconds':
-          return '500'
+        case 'file':
+          return '.tool-versions'
         default:
           return ''
       }
@@ -48,20 +45,12 @@ describe('action', () => {
     expect(runMock).toHaveReturned()
 
     // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
+    expect(debugMock).toHaveBeenCalledWith(
+      'Parsing tool-versions file: .tool-versions'
     )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
+    // Verify that the core.setOutput() function was called with the expected values
+    expect(setOutputMock).toHaveBeenCalledWith('nodejs-version', '20.11.0')
+    // Verify that the core.setFailed() and core.error() functions were not called
     expect(errorMock).not.toHaveBeenCalled()
   })
 
@@ -69,8 +58,8 @@ describe('action', () => {
     // Set the action's inputs as return values from core.getInput()
     getInputMock.mockImplementation(name => {
       switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
+        case 'file':
+          return '.not-a-file'
         default:
           return ''
       }
@@ -80,10 +69,7 @@ describe('action', () => {
     expect(runMock).toHaveReturned()
 
     // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds not a number'
-    )
+    expect(setFailedMock).toHaveBeenCalledWith('File not found: .not-a-file')
     expect(errorMock).not.toHaveBeenCalled()
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as fs from 'fs'
 
 /**
  * The main function for the action.
@@ -7,20 +7,32 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    // Get the path to the ASDF tool-versions file from input
+    const toolVersionsFilePath = core.getInput('file')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // Check if the file exists
+    if (!fs.existsSync(toolVersionsFilePath)) {
+      throw new Error(`File not found: ${toolVersionsFilePath}`)
+    }
+    core.debug(`Parsing tool-versions file: ${toolVersionsFilePath}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Read the contents of the tool-versions file
+    const toolVersionsFileContent = fs.readFileSync(
+      toolVersionsFilePath,
+      'utf8'
+    )
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // Parse the tool-versions file and set outputs
+    const lines = toolVersionsFileContent.split('\n')
+    for (const line of lines) {
+      // Skip comments and empty lines
+      if (!line.trim() || line.trim().startsWith('#')) {
+        return
+      }
+      const [tool, version] = line.split(/\s+/)
+      core.setOutput(`${tool.toLowerCase()}-version`, version)
+    }
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
